c7757cdc6324daf3baac90dd2d5a42e594634732
Zaki Khan <mohammedkhanzaki@gmail.com>
2024-11-16 15:05:06 +0530

    Adding Event Listeners &  Working with State

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,16 @@
 import Post from './Post'
 import classes from '../components/PostsList.module.css'; 
+import NewPost from './NewPost';
 
 function PostsList() {
 return (
+     <>
+     <NewPost />
  <ul className={classes.posts}>
      <Post author="zaki" body="React.js is awesome"/>
      <Post author="zaheer" body="Next.js is awesome"/>
 </ul>
+</>
 );
 }
 
