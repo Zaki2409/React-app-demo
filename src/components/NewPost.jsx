a011578bdd409951daaf3dc2fdb7f7c819c7d9f2
Zaki Khan <mohammedkhanzaki@gmail.com>
2024-11-21 14:36:57 +0530

    Handling Form Submission

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,17 +1,40 @@
 import classes from './NewPost.module.css';
+import { useState } from 'react';
 
 function NewPost(props) {
 
+  const [enteredBody , setEnteredBody ] = useState('');
+  const [enteredAuthor , setEnteredAuthor ] = useState('');
 
+  //Event Listenter Fucntion 
+ 
+  function changeBodyHandler(event) {
+    setEnteredBody(event.target.value);
+  } 
+
+  function authorBodyHandler(event) {
+      setEnteredAuthor(event.target.value);
+    } 
+
+  function submitHandler(event) {
+    event.preventDefault();
+    const postData = {
+      body : enteredBody,
+      author : enteredAuthor
+    };
+    console.log(postData);
+    props.onCancel();
+
+  }
   return (
-    <form className={classes.form}> 
+    <form className={classes.form} onSubmit={submitHandler}> 
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={props.onBodyChange}/>
+        <textarea id="body" required rows={3} onChange={changeBodyHandler}/>
       </p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required onChange={props.onAuthorChange} />
+        <input type="text" id="name" required onChange={authorBodyHandler} />
       </p>
       <p className={classes.actions}>
         <button type="button" onClick={props.onCancel}>Cancel</button>
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+
 import Post from './Post'
 import classes from '../components/PostsList.module.css'; 
 import NewPost from './NewPost';
@@ -6,30 +6,18 @@ import Modal from './modal';
 
 function PostsList({onStopPosting , isPosting}) {
 
-    const [enteredBody , setEnteredBody ] = useState('');
-    const [enteredAuthor , setEnteredAuthor ] = useState('');
-
-    //Event Listenter Fucntion 
    
-    function changeBodyHandler(event) {
-      setEnteredBody(event.target.value);
-    } 
-
-    function authorBodyHandler(event) {
-        setEnteredAuthor(event.target.value);
-      } 
 return (
      <>
      {isPosting ? <Modal onClose={onStopPosting}>
      <NewPost 
-          onBodyChange={changeBodyHandler} 
-          onAuthorChange={authorBodyHandler}
+
           onCancel={onStopPosting}
           />
       </Modal> :false}
      
  <ul className={classes.posts}>
-     <Post author={enteredAuthor} body={enteredBody}/>
+     
      <Post author="zaheer" body="Next.js is awesome"/>
 </ul>
 </>
